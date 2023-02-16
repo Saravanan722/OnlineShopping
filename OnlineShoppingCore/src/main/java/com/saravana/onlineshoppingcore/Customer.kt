bd99de180fb07427cd7ff769c0ef11a1c5c94fee
Saravanan T <saravanansaravana722@gmail.com>
2023-02-16 15:58:34 +0530

    Removed open and Add nullable type

--- OnlineShoppingCore/src/main/java/com/saravana/onlineshoppingcore/Customer.kt
+++ OnlineShoppingCore/src/main/java/com/saravana/onlineshoppingcore/Customer.kt
@@ -1,19 +1,17 @@
 package com.saravana.onlineshoppingcore
 
-open class Address(
-    var street: String?,
-    var city: String?,
-    var state: String?,
-    var pinCode: String?,
+ class Address(
+    var street: String? = null,
+    var city: String? = null,
+    var state: String? = null,
+    var pinCode: String? = null,
 )
 
 class Customer(
-    var name: String,
-    var phoneNumber: String,
-    var email: String,
-) {
-
-}
+    var name: String? = null,
+    var phoneNumber: String? = null,
+    var email: String? = null,
+)
 
 
 
