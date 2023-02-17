899bd76123cc3ee4fb0f28c37b51e28d4362f4bc
Saravanan T <saravanansaravana722@gmail.com>
2023-02-17 16:59:34 +0530

    HomeActivity and HomeActivity Class add

--- app/src/main/java/com/saravana/onlineshopping/HomeActivity.kt
+++ app/src/main/java/com/saravana/onlineshopping/HomeActivity.kt
@@ -2,10 +2,36 @@ package com.saravana.onlineshopping
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.saravana.onlineshopping.databinding.ActivityHomeBinding
+import com.saravana.onlineshopping.databinding.ActivityMainBinding
+import com.saravana.onlineshoppingcore.Admin
+import com.saravana.onlineshoppingcore.Cart
 
 class HomeActivity : AppCompatActivity() {
+
+    private val cart = Cart()
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.homeButton.setOnClickListener {
+            replaceFragment(StoreFragment())
+        }
+        binding.cartButton.setOnClickListener {
+            replaceFragment(CartFragment())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
